c27272ea2e425e4544d18b6d93d2145013c775f7
FedeSandroni <fede_4744@hotmail.com>
2024-04-25 20:05:34 -0300

    Varios
    Remuevo archivo bkp, nuevo connectionString apuntando a instancia de AWS y codigo de BD en carpeta

---
+++ EjercicioBilog/EjercicioBilog/Bilog ejercicio BD.sql
@@ -0,0 +1,31 @@
+﻿create database GestionEmpleados;
+
+﻿create table Empleados(
+	PrimaryID int not null identity(1,1),
+	ID varchar(max) not null,
+	Nombre varchar(max) not null,
+	Apellido varchar (max) not null,
+);
+
+CREATE PROCEDURE SP_AgregarEmpleado(
+	@ID varchar(max),
+    @Nombre varchar,
+    @Apellido varchar 
+)
+as
+INSERT INTO Empleados (ID, Nombre, Apellido) VALUES (@ID, @Nombre, @Apellido);
+
+CREATE PROCEDURE SP_BorrarEmpleado(
+	@ID varchar(max)
+)
+as
+delete from Empleados where LOWER(ID) = LOWER(@ID)
+
+CREATE PROCEDURE SP_ModificarEmpleado(
+	@ID varchar(max),
+    @Nombre varchar,
+    @Apellido varchar
+)
+as
+UPDATE Empleados SET Nombre = @Nombre, Apellido = @Apellido where LOWER(ID) = LOWER(@ID)
+
